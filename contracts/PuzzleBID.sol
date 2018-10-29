33e1222e5717c8ef144852f6098e493b7ca9b67b
Simon <vsiryxm@qq.com>
2018-10-29 18:50:18 +0800

    初始化

---
+++ contracts/PuzzleBID.sol
@@ -0,0 +1,416 @@
+pragma solidity ^0.4.24;
+/**
+ * @title PuzzleBID v1.0
+ *
+ */
+
+library PZBdatasets {
+
+    struct Works {
+        byte32 symbol; //作品编号
+        string name; //作品名称
+        string artistName; //归属艺术家名字
+        string artistID; //归属艺术家ID
+        uint8 debrisNum; //分割成游戏碎片总数
+        uint8 initPrice; //初始总价格
+        uint256 beginTime; //开售时间
+        uint256 endTime; //中止出售时间
+        bool isPublish; //是否上架
+        string tokenID; //对应ERC721
+        string tokenURI; //资源json {'profile':'作品简介', 'thumb':'缩略图', 'picture':'作品大图', 'copyright':'版权', 'source_file':'作品源文件', 'block_no':'区块高度'}       
+    }
+
+    struct Debris {
+        byte32 symbol; //碎片编号
+        string imageUrl; //碎片地址
+        uint256 initPrice; //初始价格
+        uint256 lastPrice; //最新价格     
+    }
+
+    struct Rules {
+           
+    }
+
+    struct Artist {
+        string artistID; //艺术家ID
+        string artistName; //归属艺术家名字
+        address ethAddress; //钱包地址
+    }
+
+    struct Player {
+        address ethAddress;   // player address
+        bytes32 name;   // player name
+        uint256 win;    // winnings vault
+        uint256 gen;    // general vault
+        uint256 aff;    // affiliate vault
+        uint256 lrnd;   // last round played
+        uint256 laff;   // last affiliate id used
+    }
+
+    struct Transaction {        
+        bytes32 worksSymbol;
+        bytes32 debrisSymbol;
+        uint256 artistID;
+        uint256 dealPrice;
+        address fromAddress;
+        address toAddress; 
+    }
+
+
+}
+
+contract PuzzleBIDevents {
+
+}
+
+contract PuzzleBID is PuzzleBIDevents {
+    using SafeMath for *;
+
+    //============================================
+    //| Game config
+    //============================================
+    string constant public name = "PuzzleBID Official";
+    string constant public symbol = "PZD";
+    uint256 constant private maxDebris = 6; //作品被分割成最大碎片数
+    uint256 constant private firstRoundBuyNum = maxDebris.mul(1/3); //首发最多能购买的碎片数
+    uint256 constant private freezeTime = 300 seconds; //购买一个作品中的一个碎片后冻结5分钟
+    uint256 constant private protectTime = 1800 seconds; //碎片保护时间30分钟
+    uint256 constant private increaseRatio = 1.1; //碎片价格调整为上一次价格的110%
+    uint256 constant private discountTime = 1 hours; //碎片开始打折时间，被购买1小时后    
+    uint256 constant private discountRatio = 0.95; //碎片价格调整为首发价格的95%
+
+    //============================================
+    //| PLAYER DATA 
+    //============================================
+    mapping (address => uint256) public pIDxAddr_;          // (addr => pID) returns player id by address
+    mapping (bytes32 => uint256) public pIDxName_;          // (name => pID) returns player id by name
+    mapping (uint256 => F3Ddatasets.Player) public plyr_;   // (pID => data) player data
+    
+    /**
+     * @dev prevents contracts from interacting with PuzzleBID 
+     */
+    modifier isHuman() {
+        address _address = msg.sender;
+        uint256 _size;
+
+        assembly {_size := extcodesize(_address)}
+        require(_size == 0, "sorry humans only");
+        _;
+    }
+
+    /**
+     * @dev sets boundaries for incoming tx
+     * 最小0.000000001ETH，最大100000ETH
+     */
+    modifier isWithinLimits(uint256 _eth) {
+        require(_eth >= 1000000000, "pocket lint: not a valid currency");
+        require(_eth <= 100000000000000000000000, "Calm down, don't stud");
+        _;    
+    }
+
+    /**
+     * @dev emergency buy uses last stored affiliate ID and team snek
+     */
+    function()
+        isActivated()
+        isHuman()
+        isWithinLimits(msg.value)
+        public
+        payable
+    {
+
+    }
+
+    /**
+     * @dev returns time left.  dont spam this, you'll ddos yourself from your node 
+     * provider
+     * -functionhash- 0xc7e284b8
+     * @return time left in seconds
+     */
+    function getTimeLeft()
+        public
+        view
+        returns(uint256)
+    {
+        // setup local rID
+        uint256 _rID = rID_;
+        
+        // grab time
+        uint256 _now = now;
+        
+        if (_now < round_[_rID].end)
+            if (_now > round_[_rID].strt + rndGap_)
+                return( (round_[_rID].end).sub(_now) );
+            else
+                return( (round_[_rID].strt + rndGap_).sub(_now) );
+        else
+            return(0);
+    }
+
+
+    /**
+     * @dev return the price buyer will pay for next 1 individual key.
+     * -functionhash- 0x018a25e8
+     * @return price for next key bought (in wei format)
+     */
+    function getBuyPrice()
+        public 
+        view 
+        returns(uint256)
+    {  
+        // setup local rID
+        uint256 _rID = rID_;
+        
+        // grab time
+        uint256 _now = now;
+        
+        // are we in a round?
+        if (_now > round_[_rID].strt + rndGap_ && (_now <= round_[_rID].end || (_now > round_[_rID].end && round_[_rID].plyr == 0)))
+            return ( (round_[_rID].keys.add(1000000000000000000)).ethRec(1000000000000000000) );
+        else // rounds over.  need price for new round
+            return ( 75000000000000 ); // init
+    }
+
+    /**
+     * @dev returns player info based on address.  if no address is given, it will 
+     * use msg.sender 
+     * -functionhash- 0xee0b5d8b
+     * @param _addr address of the player you want to lookup 
+     * @return player ID 
+     * @return player name
+     * @return keys owned (current round)
+     * @return winnings vault
+     * @return general vault 
+     * @return affiliate vault 
+	 * @return player round eth
+     */
+    function getPlayerInfoByAddress(address _addr)
+        public 
+        view 
+        returns(uint256, bytes32, uint256, uint256, uint256, uint256, uint256)
+    {
+        // setup local rID
+        uint256 _rID = rID_;
+        
+        if (_addr == address(0))
+        {
+            _addr == msg.sender;
+        }
+        uint256 _pID = pIDxAddr_[_addr];
+        
+        return
+        (
+            _pID,                               //0
+            plyr_[_pID].name,                   //1
+            plyrRnds_[_pID][_rID].keys,         //2
+            plyr_[_pID].win,                    //3
+            (plyr_[_pID].gen).add(calcUnMaskedEarnings(_pID, plyr_[_pID].lrnd)),       //4
+            plyr_[_pID].aff,                    //5
+            plyrRnds_[_pID][_rID].eth           //6
+        );
+    }
+
+    /**
+     * @dev updates round timer based on number of whole keys bought.
+     */
+    function updateTimer(uint256 _keys, uint256 _rID)
+        private
+    {
+        // grab time
+        uint256 _now = now;
+        
+        // calculate time based on number of keys bought
+        uint256 _newTime;
+        if (_now > round_[_rID].end && round_[_rID].plyr == 0)
+            _newTime = (((_keys) / (1000000000000000000)).mul(rndInc_)).add(_now);
+        else
+            _newTime = (((_keys) / (1000000000000000000)).mul(rndInc_)).add(round_[_rID].end);
+        
+        // compare to max and set new end time
+        if (_newTime < (rndMax_).add(_now))
+            round_[_rID].end = _newTime;
+        else
+            round_[_rID].end = rndMax_.add(_now);
+    }
+
+ }
+
+ /**
+ * @title SafeMath v0.1.9
+ * @dev Math operations with safety checks that throw on error
+ * change notes:  original SafeMath library from OpenZeppelin modified by Inventor
+ * - added sqrt
+ * - added sq
+ * - added pwr 
+ * - changed asserts to requires with error log outputs
+ * - removed div, its useless
+ */
+library SafeMath {
+    
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) 
+        internal 
+        pure 
+        returns (uint256 c) 
+    {
+        if (a == 0) {
+            return 0;
+        }
+        c = a * b;
+        require(c / a == b, "SafeMath mul failed");
+        return c;
+    }
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b)
+        internal
+        pure
+        returns (uint256) 
+    {
+        require(b <= a, "SafeMath sub failed");
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b)
+        internal
+        pure
+        returns (uint256 c) 
+    {
+        c = a + b;
+        require(c >= a, "SafeMath add failed");
+        return c;
+    }
+    
+    /**
+     * @dev gives square root of given x.
+     */
+    function sqrt(uint256 x)
+        internal
+        pure
+        returns (uint256 y) 
+    {
+        uint256 z = ((add(x,1)) / 2);
+        y = x;
+        while (z < y) 
+        {
+            y = z;
+            z = ((add((x / z),z)) / 2);
+        }
+    }
+    
+    /**
+     * @dev gives square. multiplies x by x
+     */
+    function sq(uint256 x)
+        internal
+        pure
+        returns (uint256)
+    {
+        return (mul(x,x));
+    }
+    
+    /**
+     * @dev x to the power of y 
+     */
+    function pwr(uint256 x, uint256 y)
+        internal 
+        pure 
+        returns (uint256)
+    {
+        if (x==0)
+            return (0);
+        else if (y==0)
+            return (1);
+        else 
+        {
+            uint256 z = x;
+            for (uint256 i=1; i < y; i++)
+                z = mul(z,x);
+            return (z);
+        }
+    }
+
+}
+
+library NameFilter {
+    /**
+     * @dev filters name strings
+     * -converts uppercase to lower case.  
+     * -makes sure it does not start/end with a space
+     * -makes sure it does not contain multiple spaces in a row
+     * -cannot be only numbers
+     * -cannot start with 0x 
+     * -restricts characters to A-Z, a-z, 0-9, and space.
+     * @return reprocessed string in bytes32 format
+     */
+    function nameFilter(string _input)
+        internal
+        pure
+        returns(bytes32)
+    {
+        bytes memory _temp = bytes(_input);
+        uint256 _length = _temp.length;
+        
+        //sorry limited to 32 characters
+        require (_length <= 32 && _length > 0, "string must be between 1 and 32 characters");
+        // make sure it doesnt start with or end with space
+        require(_temp[0] != 0x20 && _temp[_length-1] != 0x20, "string cannot start or end with space");
+        // make sure first two characters are not 0x
+        if (_temp[0] == 0x30)
+        {
+            require(_temp[1] != 0x78, "string cannot start with 0x");
+            require(_temp[1] != 0x58, "string cannot start with 0X");
+        }
+        
+        // create a bool to track if we have a non number character
+        bool _hasNonNumber;
+        
+        // convert & check
+        for (uint256 i = 0; i < _length; i++)
+        {
+            // if its uppercase A-Z
+            if (_temp[i] > 0x40 && _temp[i] < 0x5b)
+            {
+                // convert to lower case a-z
+                _temp[i] = byte(uint(_temp[i]) + 32);
+                
+                // we have a non number
+                if (_hasNonNumber == false)
+                    _hasNonNumber = true;
+            } else {
+                require
+                (
+                    // require character is a space
+                    _temp[i] == 0x20 || 
+                    // OR lowercase a-z
+                    (_temp[i] > 0x60 && _temp[i] < 0x7b) ||
+                    // or 0-9
+                    (_temp[i] > 0x2f && _temp[i] < 0x3a),
+                    "string contains invalid characters"
+                );
+                // make sure theres not 2x spaces in a row
+                if (_temp[i] == 0x20)
+                    require( _temp[i+1] != 0x20, "string cannot contain consecutive spaces");
+                
+                // see if we have a character other than a number
+                if (_hasNonNumber == false && (_temp[i] < 0x30 || _temp[i] > 0x39))
+                    _hasNonNumber = true;    
+            }
+        }
+        
+        require(_hasNonNumber == true, "string cannot be only numbers");
+        
+        bytes32 _ret;
+        assembly {
+            _ret := mload(add(_temp, 32))
+        }
+        return (_ret);
+    }
+}
